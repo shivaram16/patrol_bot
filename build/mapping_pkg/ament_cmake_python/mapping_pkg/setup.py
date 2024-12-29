5300a53fb3369874fc55e641b5388b9607092a84
shivaram16 <shiva786842@gmail.com>
2024-12-29 09:26:08 +0530

    removed unwanted folders for push

--- build/mapping_pkg/ament_cmake_python/mapping_pkg/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='mapping_pkg',
-    version='0.0.0',
-    packages=find_packages(
-        include=('mapping_pkg', 'mapping_pkg.*')),
-)
